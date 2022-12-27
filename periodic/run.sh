b935aa73a08e4aa425c80256286bfd0b136f8eab
Yuri Victorovich <yuri@FreeBSD.org>
2022-12-26 19:02:10 -0800

    Improve error handling in the periodic script

--- periodic/run.sh
+++ periodic/run.sh
@@ -59,6 +59,10 @@ fi
 ## functions
 ##
 
+git_last_commit_hash() {
+	(cd $PORTSDIR && git log -n1 --format=format:"%H")
+}
+
 db_hash() {
 	sha256 -q ports.sqlite
 }
@@ -91,10 +95,16 @@ fail() {
 	# timestamp
 	timestamp "begin"
 
-	# pull
+	# pull (remote ports repo -> local ports repo)
 	(cd $PORTSDIR && git pull) > git-pull.log 2>git-pull.err || fail "git pull failed: $(cat git-pull.err)"
 	if [ "$(cat git-pull.log)" = "Already up to date." ]; then
 		echo "no updates: nothing to import into PortsDB"
+	elif [ -z "$(cat git-pull.log)" ]; then # no exit code and no printout isn't normal for git
+		if [ -n "$(cat git-pull.err)" ]; then
+			fail "git pull failed: 'git pull' exited with no error code, no stdout printout, and this error: $(cat git-pull.err)"
+		else
+			fail "git pull failed: 'git pull' exited with no error code, no stdout or stderr printout"
+		fi
 	else
 		ANY_UPDATES=yes
 	fi
@@ -106,8 +116,8 @@ fail() {
 		echo "---end git log---"
 	fi
 
-	# update
-	if [ $ANY_UPDATES = yes ]; then
+	# update the DB (local ports repo -> DB)
+	if [ $ANY_UPDATES = yes ] || ! [ -f repo.commit.sha256 ] || [ "$(cat repo.commit.sha256)" != $(git_last_commit_hash) ]; then
 		# save DB hash
 		DB_HASH=$(db_hash)
 		# actual update
@@ -116,15 +126,17 @@ fail() {
 			echo "no updates: git commits didn't update any pkgorigins"
 			ANY_UPDATES=no
 		fi
+		# save last commit's hash that's in DB
+		git_last_commit_hash > repo.commit.sha256
 	fi
 
-	# upload
+	# upload (DB -> file hosting)
 	if [ $ANY_UPDATES = yes ] || ! [ -f ports.sqlite.sha256 ] || [ "$(cat ports.sqlite.sha256)" != $(db_hash) ]; then
 		echo "uploading ports.sqlite with sha256=$(db_hash) ..."
 		# actual upload
 		($UPLOAD_CMD ports.sqlite) > upload.log 2>upload.err || fail "upload command failed: $(cat upload.err)"
 		# save last uploaded DB hash
-		echo $(db_hash) > ports.sqlite.sha256
+		db_hash > ports.sqlite.sha256
 	fi
 
 	# timestamp
